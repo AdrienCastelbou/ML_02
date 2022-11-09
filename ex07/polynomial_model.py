a17872344ff97b5dd3f9c028a06ff04ed2192163
Adrien Castelbou <acastelbou1@gmail.com>
2022-11-09 16:36:00 +0100

    7 ok

---
+++ ex07/polynomial_model.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def add_polynomial_features(x, power):
+    try:
+        if type(x) != np.ndarray or x.shape[1] != 1 or not isinstance(power, (int, float)):
+            return None
+        return np.vander(x.reshape(-1), power + 1 , increasing=True)[:,1:]
+    except:
+        return None
+
+def main_test():
+    x = np.arange(1,6).reshape(-1, 1)
+    print(add_polynomial_features(x, 3))
+    print(add_polynomial_features(, 6))
+
+if __name__ == "__main__":
+    main_test()
