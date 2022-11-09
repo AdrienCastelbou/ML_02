a8ba1d37ef4c4b10eaa55f49ff875e98fde41685
Adrien Castelbou <acastelbou1@gmail.com>
2022-11-09 18:15:46 +0100

    try c

--- ex09/data_spliter.py
+++ ex09/data_spliter.py
@@ -1,14 +1,17 @@
 import numpy as np
 
 def data_spliter(x, y, proportion):
-    l = x.shape[0]
-    p = int(l * proportion)
-    perm = np.random.permutation(len(x))
-    s_x = x[perm]
-    s_y = y[perm]
-    x_train, y_train = s_x[:p], s_y[:p]
-    x_test, y_test =  s_x[p:, -1], s_y[p:, -1]
-    return x_train.reshape(1, -1), x_test, y_train.reshape(1, -1), y_test
+    try:
+        l = x.shape[0]
+        p = int(l * proportion)
+        perm = np.random.permutation(len(x))
+        s_x = x[perm]
+        s_y = y[perm]
+        x_train, y_train = s_x[:p], s_y[:p]
+        x_test, y_test =  s_x[p:, -1], s_y[p:, -1]
+        return x_train.reshape(1, -1), x_test, y_train.reshape(1, -1), y_test
+    except:
+        return None
 
 def main_test():
     x1 = np.array([1, 42, 300, 10, 59]).reshape((-1, 1))
